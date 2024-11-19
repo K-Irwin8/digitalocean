43a31666394c7c5ccd45922e6861927533ab1d22
K-irwin8 <Aqlo984@gmail.com>
2024-11-20 03:35:06 +0900

    imported time LAST COMMIT: WORKED

--- process_video.py
+++ process_video.py
@@ -132,6 +132,7 @@ def write_srt(translated_segments, srt_file):
 def generate_subtitle_image(txt, video_width):
     import os
     from PIL import Image, ImageDraw, ImageFont
+    import time
 
     # Use a bundled font file
     font_path = os.path.join(os.path.dirname(__file__), "fonts", "NotoSansJP-VariableFont_wght.ttf")
